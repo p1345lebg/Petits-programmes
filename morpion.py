2a831b67215a86564904f393c1e4510c9618932a
p1345lebg <126608732+p1345lebg@users.noreply.github.com>
2025-09-16 09:59:08 +0200

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -1,36 +1,41 @@
 runing = True
 in_game = False
 
-def clear_board(board):
-    board = [[False for i in range(3)] for i in range(3)]
+def clear_board():
+    return [[False for i in range(3)] for i in range(3)]
 
 
 def verify_win_from(board, x, y):
-    if board[x][y] == board[(x+1)%3][y] == board[(x+2)%3][y]:
+    if board[x][y] == board[(y+1)%3][x] == board[(y+2)%3][x]:
         return True
-    if board[x][y] == board[x][(y+1)%3] == board[x][(y+2)%3]:
+    if board[x][y] == board[y][(x+1)%3] == board[y][(x+2)%3]:
         return True
     
-    if x == y and board[x][y] == board[(x+1)%3][(y+1)%3] == board[(x+2)%3][(y+2)%3]:
+    if x == y and board[y][x] == board[(y+1)%3][(x+1)%3] == board[(y+2)%3][(x+2)%3]:
         return True
-    if x == 3-y and board[x][y] == board[(x+1)%3][(y+2)%3] == board[(x+2)%3][(y+1)%3]:
+    if x == 3-y and board[y][x] == board[(y+1)%3][(x+2)%3] == board[(y+2)%3][(x+1)%3]:
         return True
+        
+def print_board(board)
 
 board = []
 current_player = 0
 
 while runing:
     if in_game:
+        print("board :")
         for i in board:
             print(i)
         print(f"player {current_player}")
         x = int(input("x : "))%3
         y = int(input("y : "))%3
+        board[y][x]
         if verify_win_from(board, x, y):
-            print("winner is player {current_player}")
+            print(f"winner is player {current_player}")
         current_player = (current_player+1)%3
 
     else:
-        clear_board(board)
+        board = clear_board()
+        print("board cleared")
         in_game = True
     
